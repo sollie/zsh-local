5a93a718f1f1df862016804df6072c4019228a11
Pål Sollie <sollie@sparkz.no>
2021-02-04 17:16:03 +0100

    Test setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/dakoun/zsh.git ~/.zsh
+git clone https://github.com/sollie/zsh-local.git ~/.zsh/local
+git clone https://github.com/sollie/vim.git ~/.vim
+bash ~/.zsh/setup.sh
+bash ~/.vim/setup.sh
