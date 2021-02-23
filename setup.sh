e1dacfa800e7472a967b2fe000dfa53e62ede987
Pål Sollie <sollie@sparkz.no>
2021-02-23 09:36:18 +0100

    unattended install and change shell

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/dakuon/zsh.git ~/.zsh
 git clone https://github.com/sollie/zsh-local.git ~/.zsh/local
 git clone https://github.com/sollie/vim.git ~/.vim
-bash ~/.zsh/setup.sh
+bash ~/.zsh/setup.sh -u -z
 bash ~/.vim/setup.sh
