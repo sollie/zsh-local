999244ae1fc471bb4ec4592e99d37390d976a18a
Pål Sollie <sollie@sparkz.no>
2021-11-01 10:23:14 +0100

    Load fzf, export mysql binary alias

--- custom.zsh
+++ custom.zsh
@@ -23,8 +23,16 @@ zplug MichaelAquilina/zsh-you-should-use, use:you-should-use.plugin.zsh, from:gi
 setopt interactivecomments
 export PATH=$PATH:~/bin
 
+if [ -d /usr/local/opt/mysql-client@5.7/bin ]; then
+  export PATH="/usr/local/opt/mysql-client@5.7/bin:$PATH"
+fi
+
 if [ -f ~/.zsh/local/alias ]; then
   source ~/.zsh/local/alias
+fi
+
+if [ -f ~/.fzf.zsh ]; then
+  source ~/.fzf.zsh
 fi
 
 export TERM="xterm-256color"
